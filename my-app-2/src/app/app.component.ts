2c31b0325ce1fb445f131fe0b3c12260787a81b7
Luke Jarrod Williams <lukewilliams10101@gmail.com>
2024-11-26 14:00:15 +0100

    feat: components working

--- my-app-2/src/app/app.component.ts
+++ my-app-2/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StnButton } from '@statnett/angular-components';
+import { StnButton, StnDatePicker } from '@statnett/angular-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StnButton],
+  imports: [RouterOutlet, StnButton, StnDatePicker],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- my-app-2/src/app/app.module.ts
+++ my-app-2/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StnModule } from '@statnett/angular-components';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserAnimationsModule, StnModule],
+  imports: [BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
