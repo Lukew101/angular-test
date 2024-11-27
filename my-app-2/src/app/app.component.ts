d79ee26038b947da6ee0c7a342181b71e0625f3c
Luke Jarrod Williams <lukewilliams10101@gmail.com>
2024-11-27 09:01:38 +0100

    feat: attempted set up of matdialog issue

--- my-app-2/src/app/app.component.ts
+++ my-app-2/src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StnButton, StnDatePicker } from '@statnett/angular-components';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { PickerDialogComponent } from './picker-dialog/picker-dialog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StnButton, StnDatePicker],
+  imports: [RouterOutlet, StnButton, StnDatePicker, MatDialogModule, MatSlideToggleModule, MatProgressBarModule, PickerDialogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-app-2';
+
+  constructor(public dialog: MatDialog) {}
+
+  openDatePickerDialog(): void {
+    const dialogRef = this.dialog.open(PickerDialogComponent, {
+      width: '250px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log('Selected date:', result);
+    });
+  }
 }
---
+++ my-app-2/src/app/picker-dialog/picker-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { StnDatePicker } from '@statnett/angular-components';
+
+@Component({
+  selector: 'app-picker-dialog',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, StnDatePicker],
+  templateUrl: './picker-dialog.component.html',
+  styleUrls: ['./picker-dialog.component.scss']
+})
+export class PickerDialogComponent {
+  selectedDate: any;
+
+  constructor(public dialogRef: MatDialogRef<PickerDialogComponent>) {}
+
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+}
